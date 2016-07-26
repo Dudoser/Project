95ad1f0dd39fe2c02adb66db677e2289cc665a65
Vlad Babin <Vlad_Babin_2013@mail.ru>
2016-07-26 18:07:40 +0300

    migration

--- studentsdb/context_processors.py
+++ studentsdb/context_processors.py
@@ -1,7 +1,3 @@
-from .settings import PORTAL_URL
 
 def students_proc(request):
-	PORTAL_URL = (
-	{'name' : request.get_host()},
-	)
-	return {'PORTAL_URL': PORTAL_URL}
+	return {'PORTAL_URL': request.get_host()}
--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'studentsdb.urls'
 
-PORTAL_URL = '{{ PORTAL_URL.name }}'
 
 TEMPLATES = [
     {
@@ -125,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
